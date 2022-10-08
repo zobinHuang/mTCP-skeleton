5b13bbff674ff7b4b1a726ffc1f29523a29dcd9d
zobinHuang <zobin1999@gmail.com>
2022-10-08 23:33:42 +0800

    fixed(dpdk-iface-kmod): fix some bugs in kernel module

--- third_party/dpdk-iface-kmod/.dpdk_iface.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/zobin/projects/mTCP-app/third_party/dpdk-iface-kmod/dpdk_iface.mod := { echo  /home/zobin/projects/mTCP-app/third_party/dpdk-iface-kmod/dpdk_iface.o;  echo; } > /home/zobin/projects/mTCP-app/third_party/dpdk-iface-kmod/dpdk_iface.mod
--- third_party/dpdk-iface-kmod/dpdk_iface.mod
+++
@@ -1,2 +0,0 @@
-/home/zobin/projects/mTCP-app/third_party/dpdk-iface-kmod/dpdk_iface.o
-
